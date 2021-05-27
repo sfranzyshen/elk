04eb608b50cb092e7abc13cb3d1986ed8309e630
Scotty Franzyshen <sfranzyshen@gmail.com>
2021-05-27 13:56:57 -0700

    Update elk.c

--- elk.c
+++ elk.c
@@ -916,7 +916,6 @@ static jsval_t do_logical_or(struct js *js, jsval_t l, jsval_t r) {
   return mkval(T_BOOL, js_truthy(js, r) ? 1 : 0);
 }
 
-#ifdef JS_EXTRA
 static jsval_t do_q(struct js *js, jsval_t l, jsval_t rhs) {
   if (js->flags & F_NOEXEC) return rhs;
   if (!js_truthy(js, l)) js->flags |= F_NOEXEC;
@@ -928,7 +927,6 @@ static jsval_t do_colon(struct js *js, jsval_t lhs, jsval_t rhs) {
   if (f) js->flags &= ~F_NOEXEC;
   return f ? rhs : lhs;
 }
-#endif
 
 // clang-format off
 static jsval_t do_op(struct js *js, uint8_t op, jsval_t lhs, jsval_t rhs) {
@@ -936,10 +934,8 @@ static jsval_t do_op(struct js *js, uint8_t op, jsval_t lhs, jsval_t rhs) {
   //printf("OP %d %d %d\n", op, vtype(lhs), vtype(r));
   if (is_assign(op) && vtype(lhs) != T_PROP) return js_err(js, "bad lhs");
   switch (op) {
-#ifdef JS_EXTRA
     case TOK_Q:       return do_q(js, l, rhs);
     case TOK_COLON:   return do_colon(js, lhs, rhs);
-#endif
     case TOK_LAND:    return mkval(T_BOOL, js_truthy(js, l) && js_truthy(js, r) ? 1 : 0);
     case TOK_LOR:     return do_logical_or(js, l, r);
     case TOK_TYPEOF:  return mkstr(js, typestr(vtype(r)), strlen(typestr(vtype(r))));
